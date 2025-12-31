6faebdc429b32867f8ba4e64798abebaac67c3c8
Emmanuel <146569162+emmanuel-debrah2023@users.noreply.github.com>
2025-12-31 23:27:32 +0000

    Merge pull request #1
    * Added essential libraries and plugins for future development
    
    * First commit.

---
+++ shared/src/commonMain/kotlin/utils/Navigation.kt
@@ -0,0 +1,13 @@
+package utils
+
+/**
+ * Navigation routes for the app
+ */
+object Routes {
+    const val HOME = "home"
+    const val FRAGRANCE_DETAIL = "fragrance_detail/{fragranceId}"
+    const val PROFILE = "profile"
+    const val SEARCH = "search"
+    
+    fun fragranceDetail(fragranceId: String) = "fragrance_detail/$fragranceId"
+}
