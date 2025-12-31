52398a8a73c0d623a5f3e2de19f809bf0053ec84
emmanuel.debrah <emmanuel.debrah@tui.co.uk>
2025-12-31 23:07:42 +0000

    Add home screen with fragrance cards and navigation

--- shared/src/jvmMain/kotlin/org/scent/project/Platform.kt
+++ shared/src/jvmMain/kotlin/org/scent/project/Platform.kt
@@ -1,12 +1,5 @@
 package org.scent.project
 
-import androidx.compose.runtime.Composable
-
 actual fun getPlatform(): Platform = object : Platform {
     override val name: String = "JVM"
-}
-
-@Composable
-actual fun PlatformSpecificHomeScreen() {
-    // No-op for now
 }
